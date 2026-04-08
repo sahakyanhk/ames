969e299255d9b8855b4cb41e801771f42208943b
Harutyun Sahakyan <sahakyanhk@gmail.com>
2026-04-08 16:08:04 -0400

    esm_example

---
+++ bash_helpers/run_simulacrum.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+
+python "$REPO_ROOT/src/ames.py" --iseq1 protein:randoms:100:evolv \
+                                -pm rso \
+                                -ps 100 -ng 10000 \
+                                --engine simulacrum \
+                                --nobackup \
+                                -b0 0.1 \
+                                -ann \
+                                -bt 10.0 \
+                                -ann_s 250 \
+                                -ann_e 7500 \
+                                -o "$1"
+
+
+python "$REPO_ROOT/src/visual_ames.py" -l "$1"/progress.log 
+
